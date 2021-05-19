108afd9bb57fbb9c1f727c52f506ab142ea775f0
hecthorl <hlvm03@gmail.com>
2021-05-19 14:52:48 -0500

    arreglando errores de compatibilidad de firebase con next

--- pages/status/[id]/index.jsx
+++ pages/status/[id]/index.jsx
@@ -1,9 +1,9 @@
 import Devit from 'components/Devit'
+import { firestore } from 'firebase/admin'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 const DevitPage = props => {
-   // console.log(props, 'page[id]')
    const { isFallback } = useRouter()
    if (isFallback) return <h1>Cargando...</h1>
    return (
@@ -18,17 +18,30 @@ const DevitPage = props => {
 
 export const getStaticPaths = async () => {
    return {
-      paths: [{ params: { id: 'asdasd' } }],
+      paths: [{ params: { id: 'g8ldtalv1ENybubFTgKt' } }],
       fallback: !false
    }
 }
 
 export const getStaticProps = context => {
    const { id } = context.params
+   return firestore
+      .collection('devits')
+      .doc(id)
+      .get()
+      .then(doc => {
+         const data = doc.data()
+         const id = doc.id
 
-   return fetch(`http://localhost:3000/api/devits/${id}`)
-      .then(res => res.json())
-      .then(devit => ({ props: { ...devit } }))
+         const props = {
+            ...data,
+            id
+         }
+         return { props }
+      })
+      .catch(() => {
+         return { props: {} }
+      })
 }
 
 export default DevitPage
