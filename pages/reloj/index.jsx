48cc84fa8c8d86a199f15c781650a65cbfc477cc
hecthorl <hlvm03@gmail.com>
2021-06-07 12:30:10 -0500

    resolviendo re-renderizado de pagina reloj 4/5

--- pages/reloj/index.jsx
+++ pages/reloj/index.jsx
@@ -1,18 +1,35 @@
 import loadingRelojStyles from 'components/loadingRelojStyles'
 import Head from 'next/head'
-import useDate from '../../hooks/useDate'
+import { useEffect, useRef } from 'react'
+
+const intl = () =>
+   new Intl.DateTimeFormat('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hourCycle: 'h12'
+      // timeZone: 'America/Lima', => Default para mi :v
+      // timeZone: 'Europe/Madrid',
+   }).format(Date.now())
 
 const Reloj = () => {
-   const hora = useDate()
+   const time = useRef()
+
+   useEffect(() => {
+      const clear = setInterval(() => {
+         time.current.innerHTML = intl()
+      }, 1e3)
+      return () => clearInterval(clear)
+   }, [])
 
    return (
       <div>
          <Head>
-            <title>{hora || 'Cargando....'}</title>
+            <title>Reloj</title>
          </Head>
          <h1>Reloj</h1>
-         <div className="container">
-            {!hora ? <div className="loading"></div> : <span>{hora}</span>}
+         <div ref={time} className="container">
+            <div className="loading"></div>
          </div>
          <style jsx>{loadingRelojStyles}</style>
       </div>
