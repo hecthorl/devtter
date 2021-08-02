e44e6243cbd59a1fa0fe6bd6c3a8b3f5dfb0fded
hecthorl <hlvm03@gmail.com>
2021-08-02 09:02:57 -0500

    experimental con article

--- components/Devit/index.jsx
+++ components/Devit/index.jsx
@@ -5,84 +5,84 @@ import CommentBtn from './CommentBtn'
 import LikeBtn from './LikeBtn'
 import { BsThreeDots } from 'react-icons/bs'
 import DevitMedia from 'components/DevitMedia'
+import { useRouter } from 'next/router'
 
 const Devit = ({ avatar, content, username, createdAt, img, id }) => {
    const [avatarImg, setAvatarImg] = useState(false)
 
    const date = useTimeAgo(createdAt)
-   // const { push } = useRouter()
+   const { push } = useRouter()
 
-   // const handleClickSingleTweet = event => {
-   //    event.preventDefault()
-   //    push(`/status/${id}`)
-   // }
+   const handleClickSingleTweet = event => {
+      event.preventDefault()
+      push(`/status/${id}`)
+   }
 
    const toimageAv = avatarImg ? 'opacity-100' : 'opacity-0'
    const toFallbackImgAv = avatarImg ? 'opacity-0' : 'opacity-100'
 
    return (
-      <Link href={`/status/${id}`}>
-         <a
-            // onClick={handleClickSingleTweet}
-            className="px-4 flex gap-x-3 py-3 border-b border-secondary hover:bg-[#ffffff0d] cursor-pointer transition-colors duration-200"
+      // <Link href={`/status/${id}`}>
+      <article
+         onClick={handleClickSingleTweet}
+         className="px-4 flex gap-x-3 py-3 border-b border-secondary hover:bg-[#ffffff0d] cursor-pointer transition-colors duration-200"
+      >
+         <div
+            onClick={e => e.stopPropagation()}
+            className="relative max-h-[48px] min-w-[48px] rounded-full overflow-hidden"
          >
+            <img
+               onLoad={() => setAvatarImg(true)}
+               loading="lazy"
+               className={'h-12 ' + toimageAv}
+               src={avatar}
+               alt={'current image profile of ' + username}
+            />
             <div
-               onClick={e => e.stopPropagation()}
-               className="relative max-h-[48px] min-w-[48px] rounded-full overflow-hidden"
-            >
-               <img
-                  onLoad={() => setAvatarImg(true)}
-                  loading="lazy"
-                  className={'h-12 ' + toimageAv}
-                  src={avatar}
-                  alt={'current image profile of ' + username}
-               />
-               <div
-                  className={
-                     'absolute w-full h-full top-0 bg-blue-600 ' +
-                     toFallbackImgAv
-                  }
-               ></div>
-            </div>
-            <div className="w-full">
-               <div className="flex justify-between">
-                  <div className="flex gap-x-2">
-                     <h2 className="font-bold hover:underline">{username}</h2>
-                     <div
-                        onClick={e => e.stopPropagation()}
-                        className="text-white space-x-1 text-opacity-50"
-                     >
-                        <span>@{username.replace(' ', '_')}</span>
-                        <span>•</span>
-                        <time>{date.replace(/hace | /g, '')}</time>
-                     </div>
-                  </div>
-                  <div onClick={e => e.stopPropagation()}>
-                     <button>
-                        <BsThreeDots />
-                     </button>
+               className={
+                  'absolute w-full h-full top-0 bg-blue-600 ' + toFallbackImgAv
+               }
+            ></div>
+         </div>
+         <div className="w-full">
+            <div className="flex justify-between">
+               <div className="flex gap-x-2">
+                  <h2 className="font-bold hover:underline">{username}</h2>
+                  <div
+                     onClick={e => e.stopPropagation()}
+                     className="text-white space-x-1 text-opacity-50"
+                  >
+                     <span>@{username.replace(' ', '_')}</span>
+                     <span>•</span>
+                     <time>{date.replace(/hace | /g, '')}</time>
                   </div>
                </div>
-               <div className="space-y-2">
-                  <div className="pb-3">
-                     <p className="text-base pb-2">{content}</p>
-                     <DevitMedia img={img} />
-                  </div>
-                  <div className="flex max-w-[425px] w-full justify-between text-sm text-[#8899a6]">
-                     <CommentBtn />
-                     <button onClick={e => e.stopPropagation()}>
-                        <span>🧁</span>
-                        <span>10</span>
-                     </button>
-                     <LikeBtn />
-                     <button onClick={e => e.stopPropagation()}>
-                        <span>🥤</span>
-                     </button>
-                  </div>
+               <div onClick={e => e.stopPropagation()}>
+                  <button>
+                     <BsThreeDots />
+                  </button>
                </div>
             </div>
-         </a>
-      </Link>
+            <div className="space-y-2">
+               <div className="pb-3">
+                  <p className="text-base pb-2">{content}</p>
+                  <DevitMedia img={img} />
+               </div>
+               <div className="flex max-w-[425px] w-full justify-between text-sm text-[#8899a6]">
+                  <CommentBtn />
+                  <button onClick={e => e.stopPropagation()}>
+                     <span>🧁</span>
+                     <span>10</span>
+                  </button>
+                  <LikeBtn />
+                  <button onClick={e => e.stopPropagation()}>
+                     <span>🥤</span>
+                  </button>
+               </div>
+            </div>
+         </div>
+      </article>
+      // </Link>
    )
 }
 
