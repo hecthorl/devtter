79032914934bc31aac65d8d0a10dbccbf386f475
hector <hlvm03@gmail.com>
2022-01-28 20:34:53 -0500

    xd

---
+++ components/DevitInput/index.jsx
@@ -0,0 +1,64 @@
+import {
+   Avatar,
+   Box,
+   Button,
+   Flex,
+   HStack,
+   Text,
+   VStack
+} from '@chakra-ui/react'
+import Textarea from 'react-textarea-autosize'
+import useUserAuth from 'hooks/useAuthUser'
+
+const DevitInput = () => {
+   const { userData } = useUserAuth()
+   return (
+      <Flex
+         px="16px"
+         pb="12px"
+         borderBottomColor="#38444d"
+         borderBottomWidth="1px"
+      >
+         <Avatar
+            mr="12px"
+            src={userData.user.image}
+            name={userData.user.name}
+         />
+         <VStack width="full" spacing={0}>
+            <Textarea
+               placeholder="¿Qué esta pasando?"
+               minRows={2}
+               style={{
+                  width: '100%',
+                  background: '#15202b',
+                  resize: 'none',
+                  color: 'white',
+                  outline: 'none',
+                  fontSize: '20px'
+               }}
+            />
+            <Box
+               w="full"
+               borderBottomColor="#38444d"
+               my={1}
+               borderBottomWidth="1px"
+            ></Box>
+            {/* <Flex>anywya</Flex> */}
+            <Flex pt="12px" justify="space-between" align="center" w="full">
+               <HStack spacing={6}>
+                  <Box>🐱‍🏍</Box>
+                  <Box>🐱‍👤</Box>
+                  <Box>🐱‍💻</Box>
+                  <Box>🐱‍🐉</Box>
+                  <Box>🐱‍👓</Box>
+               </HStack>
+               <Button h="36px" rounded="full" bg="orange.700">
+                  <Text textColor="white"> Devittear</Text>
+               </Button>
+            </Flex>
+         </VStack>
+      </Flex>
+   )
+}
+
+export default DevitInput
--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -1,12 +1,14 @@
 import { Flex } from '@chakra-ui/react'
 import useTimeline from 'hooks/useTimeline'
 import Devit from './Devit'
+import DevitInput from './DevitInput'
 
 const Timeline = () => {
    const timeline = useTimeline()
 
    return (
       <Flex as="article" direction="column" w="full">
+         <DevitInput />
          {timeline.length
             ? timeline.map(devit => <Devit key={devit.id} devit={devit} />)
             : 'esperando'}
